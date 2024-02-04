7601f5ef0a71adbfda109ffeb9a9fcc4c5f99003
llt <418797644@qq.com>
2024-02-04 23:23:55 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
-      >
+      <body className={cn('font-sans antialiased', fontSans.variable)}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import LoginButton from '@/components/auth/login-button'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className="min-h-screen flex flex-col justify-center items-center bg-slate-100">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-bold drop-shadow-md">Auth</h1>
+        <p className="text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button size="lg">Sign in</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   )
 }
