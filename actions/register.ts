7bf0d6205d3a6daa1e33c80395a66a0f3203d3f6
llt <418797644@qq.com>
2024-02-07 22:19:05 +0800

    update

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from '@/schemas'
 import { z } from 'zod'
+import bcrypt from 'bcryptjs'
+import prisma from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -10,7 +13,28 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
     return { error: 'Invalid fields!' }
   }
 
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return {
+      error: 'Email already in use!',
+    }
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  // TODO: Send verification token email
+
   return {
-    success: 'Email sent!',
+    success: 'User created!',
   }
 }
