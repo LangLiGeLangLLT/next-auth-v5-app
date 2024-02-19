e3fd2d939cea53ab1ca3bfaebe3642b3a2a528c0
llt <418797644@qq.com>
2024-02-19 22:13:48 +0800

    update

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 'use server'
 
+import { signIn } from '@/auth'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 import { LoginSchema } from '@/schemas'
+import { AuthError } from 'next-auth'
 import { z } from 'zod'
 
 export async function login(values: z.infer<typeof LoginSchema>) {
@@ -10,7 +13,28 @@ export async function login(values: z.infer<typeof LoginSchema>) {
     return { error: 'Invalid fields!' }
   }
 
-  return {
-    success: 'Email sent!',
+  const { email, password } = validatedFields.data
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return {
+            error: 'Invalid credentials!',
+          }
+        default:
+          return {
+            error: 'Something went wrong!',
+          }
+      }
+    }
+
+    throw error
   }
 }
