412608b5228685c91d20af0f817a86dd6100325c
llt <418797644@qq.com>
2024-02-29 23:48:39 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
 import { auth } from '@/auth'
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from '@/components/ui/sonner'
+
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +25,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="font-sans antialiased">
+      <body className={cn('font-sans antialiased', fontSans.variable)}>
         <SessionProvider session={session}>
           {children}
           <Toaster />
